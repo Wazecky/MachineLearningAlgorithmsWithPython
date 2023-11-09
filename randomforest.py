847ee55511839702acfb85be0dbcaf138d0f9b37
Alvan Wazecky <alvan55zecky@gmail.com>
2023-11-10 01:06:10 +0300

    Implemented Random Forest

---
+++ randomforest.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, lr=0.01, num_iter=100000, fit_intercept=True, verbose=False):
+        self.lr = lr
+        self.num_iter = num_iter
+        self.fit_intercept = fit_intercept
+        self.verbose = verbose
+    
+    def __add_intercept(self, X):
+        intercept = np.ones((X.shape[0], 1))
+        return np.concatenate((intercept, X), axis=1)
+    
+    def __sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    
+    def __loss(self, h, y):
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+        
+        self.theta = np.zeros(X.shape[1])
+        
+        for i in range(self.num_iter):
+            z = np.dot(X, self.theta)
+            h = self.__sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            self.theta -= self.lr * gradient
+            
+            if(self.verbose == True and i % 10000 == 0):
+                z = np.dot(X, self.theta)
+                h = self.__sigmoid(z)
+                print(f'loss: {self.__loss(h, y)} \t')
+    
+    def predict_prob(self, X):
+        if self.fit_intercept:
+            X = self.__add_intercept(X)
+    
+        return self.__sigmoid(np.dot(X, self.theta))
+    
+    def predict(self, X, threshold):
+        return self.predict_prob(X) >= threshold
+
+# Let's create some dummy data for demonstration
+X_train = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
+y_train = np.array([0, 0, 1, 1, 1])
+
+# Create an instance of LogisticRegression
+model = LogisticRegression(lr=0.01, num_iter=100000)
+
+# Fit the model to the training data
+model.fit(X_train, y_train)
+
+# Make a prediction
+print("Prediction for [5, 6]:", model.predict(np.array([[5, 6]]), threshold=0.5))
